314dfb24ff493f656312bdfe74ca231bef667826
Joseph Ameh <joeoameh@gmail.com>
2025-04-04 16:17:25 +0100

    Created a basic GUI without interaction, day 2025-04-04

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Reads a text file and return the list of
     to do items.
     """
@@ -6,7 +8,7 @@ def get_todos(filepath="todos.txt"):
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Writes the to-do items list in the text file. """
     with open(filepath, "w") as file_local:
         file_local.writelines(todos_arg)
